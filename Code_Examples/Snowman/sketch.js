bbffdc181a4ac019da595b8d912ef717c1efaf34
bsk <bensamuelskalow@gmail.com>
2016-05-12 08:23:01 -0400

    snowman!

--- Code_Examples/Snowman/sketch.js
+++ Code_Examples/Snowman/sketch.js
@@ -1,15 +1,26 @@
 function setup() {
-    createCanvas(200, 200);
+    createCanvas(400, 220);
 }
 
 function draw() {
-    background('grey')
-    strokeWeight(3)
-    line(130,50,175,10)
-    line(70,50,25,10)
-ellipse(100,90,100,100)
-ellipse(100,60,70,70)
-ellipse(100,30,40,40)
+    // background('grey')
 
+    strokeWeight(3);
+    line(135, 70, 175, 43);
+    line(65, 70, 25, 43);
+    ellipse(100, 110, 100, 100);
+    ellipse(100, 80, 70, 70);
+    ellipse(100, 50, 40, 40);
+    strokeWeight(1);
+    fill('#FF6600');;
+    triangle(98,60,104,58,100+mouseX/20,70+mouseY/20);
+    fill('white');
+    ellipse(92,50,5,5);
+    ellipse(108,50,5,5);
+    textSize(24);
+    fill('black')
+    textFont('Proxima Nova')
+    textStyle('italic')
+    text("Do you want to build a me?",10,200);
 
 }
