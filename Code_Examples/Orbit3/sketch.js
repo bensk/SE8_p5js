26836779fcf7cb445898b261f45b8b874919fd21
Ben Samuels-Kalow <bensamuelskalow@gmail.com>
2016-05-16 11:37:47 -0400

    orbits

--- Code_Examples/Orbit3/sketch.js
+++ Code_Examples/Orbit3/sketch.js
@@ -1,38 +1,54 @@
 function setup() {
-	createCanvas(windowWidth, windowHeight)
+  createCanvas(windowWidth, windowHeight)
 }
 var x = 0
 var y = 5
 var countingUp = 1;
 
 function draw() {
-	background('white')
-	text("Mouse: " + mouseX + ", " + mouseY, 0, 10)
-	text("Window: " + windowWidth + ", " + windowHeight, 0, 20)
-	text(x, 0, 30)
+  background('white')
+  text("Mouse: " + mouseX + ", " + mouseY, 0, 10)
+  text("Window: " + windowWidth + ", " + windowHeight, 0, 20)
+  text(x, 0, 30)
   fill('yellow')
-	ellipse(windowWidth/2, windowHeight/2, abs(y-37), 37)
-	
-	translate(width / 2, height / 2);
-	strokeWeight(y)
-	rotate((PI / 3.0) * x / 1000);
-	point(windowWidth/4, windowHeight/4);
-	y += (1 * countingUp);
-	strokeWeight(8)
-	rotate((PI / 3.0) * x / 100);
-	point(windowWidth/8, windowHeight/8);
-	rotate((PI / 3.0) * x / 100);
+  ellipse(windowWidth / 2, windowHeight / 2, abs(y - 37), 37)
 
-	if (y == 75 || y == 5) {
-		countingUp *= -1;
-	}
-	rotate(PI);
-	point(windowWidth/15, windowHeight/15);
-	
-	
-	x = x + 10
+  translate(width / 2, height / 2);
+
+  strokeWeight(y)
+  rotate((PI / 3.0) * x / 1000);
+  point(windowWidth / 4, windowHeight / 4);
+  y += (1 * countingUp);
+  strokeWeight(8)
+  rotate(PI)
+  point(windowWidth / 6, windowHeight / 6);
+  rotate((PI / 3.0) * x / 100);
+  point(windowWidth / 8, windowHeight / 8);
+  rotate(PI)
+  point(windowWidth / 15, windowHeight / 10);
+  rotate((PI / 3.0) * x / 100);
+
+  if (y == 75 || y == 5) {
+    countingUp *= -1;
+  }
+  rotate(PI);
+  point(windowWidth / 15, windowHeight / 15);
+  rotate(PI);
+  point(windowWidth / 30, windowHeight / 30);
+
+
+  x = x + 10
+ 
 }
 
-function windowResized(){
-  resizeCanvas(windowWidth,windowHeight)
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight)
+}
+
+function mousePressed() {
+  noLoop();
+}
+
+function mouseReleased() {
+  loop();
 }
