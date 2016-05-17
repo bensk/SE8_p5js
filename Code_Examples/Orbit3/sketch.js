0f58764522d7703fd97615f12ecbf4d3357fdffb
Ben Samuels-Kalow <bensamuelskalow@gmail.com>
2016-05-17 11:27:01 -0400

    orbits

--- Code_Examples/Orbit3/sketch.js
+++ Code_Examples/Orbit3/sketch.js
@@ -6,25 +6,32 @@ var y = 5
 var countingUp = 1;
 
 function draw() {
-  background('white')
+  background(5)
+  fill('white')
   text("Mouse: " + mouseX + ", " + mouseY, 0, 10)
   text("Window: " + windowWidth + ", " + windowHeight, 0, 20)
   text(x, 0, 30)
-  fill('yellow')
-  ellipse(windowWidth / 2, windowHeight / 2, abs(y - 37), 37)
+  fill('#FFF400')
+  ellipse(windowWidth / 2, windowHeight / 2, abs(y + 1), abs(y + 1))
+  
+  
+  
 
   translate(width / 2, height / 2);
-
+stroke('#E32173')
   strokeWeight(y)
   rotate((PI / 3.0) * x / 1000);
   point(windowWidth / 4, windowHeight / 4);
   y += (1 * countingUp);
   strokeWeight(8)
   rotate(PI)
+  stroke('#70E6BF')
   point(windowWidth / 6, windowHeight / 6);
   rotate((PI / 3.0) * x / 100);
+  stroke('#EDB538')
   point(windowWidth / 8, windowHeight / 8);
   rotate(PI)
+  stroke('#FF6600')
   point(windowWidth / 15, windowHeight / 10);
   rotate((PI / 3.0) * x / 100);
 
@@ -32,8 +39,10 @@ function draw() {
     countingUp *= -1;
   }
   rotate(PI);
+  stroke('#FFEBFF')
   point(windowWidth / 15, windowHeight / 15);
   rotate(PI);
+  stroke('#4FC2A1')
   point(windowWidth / 30, windowHeight / 30);
 
 
