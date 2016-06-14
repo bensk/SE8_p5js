b71d48b75e1638925018d88ab564ffd35a4e896f
Ben Samuels-Kalow <bensamuelskalow@gmail.com>
2016-06-14 14:46:04 -0400

    ⚡️ Fixed it!

--- Code_Examples/PongV1/sketch.js
+++ Code_Examples/PongV1/sketch.js
@@ -3,9 +3,10 @@ var y
 var speed = 10
 var xDirection = 1
 var yDirection = 1
+score = 0
 
 function setup() {
-  createCanvas(500, 500)
+  createCanvas(windowWidth, windowHeight)
   x = 20
   y = 20
 }
@@ -13,16 +14,23 @@ function setup() {
 function draw() {
   noCursor()
   background('white')
+  textSize(32)
+  text(score, 40, 40)
   ellipse(x, y, 20, 20)
-  rect(450, mouseY, 20, 80)
+  rect(windowWidth-50, mouseY, 20, 80)
   x = x + xDirection * speed
   y = y + yDirection * speed
-  if (x >=450 && y>=mouseY && y <=mouseY+80 || x <=0) {
+  if (x >= windowWidth-50 && y >= mouseY && y <= mouseY + 80) { // removed || so score would work correctly
     xDirection = xDirection * -1;
+    score = score + 1
+  } else if (x <= 0) {
+    xDirection = xDirection * -1;
+
   } else if (y >= height || y <= 0) {
     yDirection = yDirection * -1;
-  } else if (x>=windowWidth){
-    x = 20
-    y = 20
+  } else if (x >= windowWidth) {
+    x = 10
+    y = random(0, windowHeight)
+    score = 0
   }
 }
